763b513bf4b93a4046b02766abee8844aa0f4191
ArjanDeo <arjandeo@bluecomet-it.com>
2024-04-06 22:42:14 +0100

    Revert "Optimized API. Currently rewriting webapp in blazor."
    This reverts commit c575c841f0b5735c594b56d031948a17275ad185.

--- API/Controllers/GeneralController.cs
+++ API/Controllers/GeneralController.cs
@@ -1,13 +1,8 @@
 ﻿using DataAccess;
 using DataAccess.Tables;
-using LazyCache;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Models.BattleNet.OAuth;
-using Models.BattleNet.Public;
-using Models.Constants;
 using Models.WhatChores.API;
-using Pathoschild.Http.Client;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace API.Controllers
@@ -17,8 +12,6 @@ namespace API.Controllers
     public class GeneralController(WhatChoresDbContext context) : ControllerBase
     {
         private readonly WhatChoresDbContext _context = context;
-        private readonly IAppCache cache = new CachingService();
-
 
         [HttpGet("realms")]
         [SwaggerOperation(
@@ -36,7 +29,7 @@ namespace API.Controllers
             return realmNames;
         }
         [HttpGet("class")]
-        public async Task<ActionResult<List<tbl_ClassData>>> GetClasses(bool getColor)
+        public async Task<ActionResult<List<tbl_ClassData>>> GetRealmNames(bool getColor)
         {
             if (getColor)
             {
@@ -55,51 +48,8 @@ namespace API.Controllers
                 return Ok(realmNames);
             }
         }
-        [HttpGet("wowtoken")]
-        public async Task<WoWTokenPriceModel> GetWoWToken()
-        {           
-            return await cache.GetOrAddAsync("GetWoWToken", async () =>
-            {
-                FluentClient client = new();
 
-                Dictionary<string, string> ApiPayload = new()
-                {
-                    ["region"] = "us",
-                    ["namespace"] = "dynamic-us",
-                    ["locale"] = "en_US"
-                };
-                if (AppConstants.AccessToken == null)
-                {
-                    Dictionary<string, string> AccessTokenPayload = new()
-                    {
-                        [":region"] = "us",
-                        ["grant_type"] = "client_credentials"
-                    };
 
-                    IResponse TokenResponse = await client
-                       .PostAsync("https://oauth.battle.net/oauth/token")
-                       .WithBody(p => p.FormUrlEncoded(AccessTokenPayload))
-                       .WithBasicAuthentication("97cd06eb96aa40e498af899ccfe65129", "o28W9L8PuJdl5AkKk44VJRZuDrYOzyYS");
 
-                    AccessTokenModel TokenResponseData = await TokenResponse.As<AccessTokenModel>();
-                    AppConstants.AccessToken = TokenResponseData;
-                }
-                IResponse ResponseData = await client
-                    .GetAsync("https://us.api.blizzard.com/data/wow/token/index")
-                    .WithOptions(ignoreHttpErrors: true)
-                    .WithArguments(ApiPayload)
-                    .WithBearerAuthentication(AppConstants.AccessToken.access_token);
-
-                WoWTokenPriceModel ResponseDataModel = await ResponseData.As<WoWTokenPriceModel>();
-
-                // The price of wow token is, for some reason, returned with unnecessary 0's at the end (e.g. 360541000)
-                // So here I divide it by 10000 to remove the last 3.
-                // This will need to be changed in the extremely unlikely event that the token reaches prices of over 1 million or less than 100 thousand.
-                ResponseDataModel.price /= 10000;
-
-                return ResponseDataModel;
-            }, TimeSpan.FromMinutes(60));
-
-        }
     }
 }
--- API/Controllers/MythicPlusController.cs
+++ API/Controllers/MythicPlusController.cs
@@ -2,13 +2,11 @@
 using DataAccess.Tables;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Models.WhatChores.API;
 
 namespace API.Controllers
 {
     [Route("api/v1/mythicplus")]
     [ApiController]
-    [Produces("application/json")]
     public class MythicPlusController : ControllerBase
     {
         private readonly WhatChoresDbContext _context;
@@ -17,57 +15,31 @@ namespace API.Controllers
         {
             _context = context;
         }
-        /// <summary>
-        /// Gets the vault reward ilvl for a specific M+ key level.
-        /// </summary>
-        /// <param name="keyLevel"></param>
-        /// <response code="200">Returns the vault reward ilvl for provided key level (all key levels shown if none specified)</response>
-        /// <response code="400">If an invalid key level was entered. (NaN or below 2)</response>
-        /// <response code="404">If the key level wasn't found</response>
-        [HttpGet("keystone-vault-reward")]
-        [ProducesResponseType(typeof(tbl_MythicPlusValues), 200)]
-        [ProducesResponseType(typeof(Error), 400)]
-        [ProducesResponseType(typeof(Error), 404)]
-        public async Task<ActionResult<List<tbl_MythicPlusValues>>> Get(int? keyLevel)
+
+        [HttpGet("vault-ilvl-by-keylevel")]       
+        public async Task<ActionResult<object>> Get(int? keyLevel)
         {
+            if (keyLevel.HasValue && (keyLevel < 2 || keyLevel > 20))
+            {
+                return BadRequest("Key level should be between 2 and 20.");
+            }
+
             IQueryable<tbl_MythicPlusValues> query = _context.tbl_MythicPlusValues;
 
-            // if keyLevel is NaN or less than 2
-            if (keyLevel.HasValue && (keyLevel < 2))
+            if (keyLevel != null)
             {
-                Error error = new()
-                {
-                    ErrorCode = 400,
-                    ErrorMessage = "Key level must be 2 or higher."
-                };
-                return BadRequest(error);
+                query = query.Where(k => k.KeyLevel == keyLevel);
             }
 
-            if (keyLevel.HasValue && keyLevel < 20)
+            var data = await query.ToDictionaryAsync(k => "+" + k.KeyLevel, i => i.ItemLevel);
+            var formattedData = new
             {
-                query = query.Where(k => k.KeyLevel == keyLevel.Value);
-                var data = await query.FirstOrDefaultAsync();
-                return Ok(new { keyLevel, itemLevel = data.ItemLevel });
+                vaultIlvl = data,
+            };
 
-            }
-
-            // if keyLevel is greater than 20
-            else if (keyLevel.HasValue && (keyLevel > 20))
-            {
-                query = query.Where(k => k.KeyLevel == 20);
-                var data = await query.FirstOrDefaultAsync();              
-               
-                return Ok(new { keyLevel, itemLevel = data.ItemLevel });
-            }         
-
-            // if there was an issue finding data in the database (very bad)
-            if (query == null)
-            {
-                return NotFound(); 
-            }
-
-            return Ok(query);
+            return Ok(formattedData);
         }
+
 
     }
 }
